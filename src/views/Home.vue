702d1d1537cc53a340c9080500e6d26e686e36c0
Purvesh <purvesh.patel@digistorm.com>
2020-05-21 14:17:13 +1000

    very basic vue setup

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="AvoRed logo" height="50"  src="../assets/logo.svg">
+    <HelloWorld msg="Welcome to Laravel Headless Ecommerce App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/ApolloExample.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
